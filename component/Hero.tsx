a6e56c7f93595cbc8c0c56cee941c2ac1203245d
MD Rafiul Islam Rifat <rafiul2071@gmail.com>
2025-07-31 00:44:53 +0600

    framer motion update

--- component/Hero.tsx
+++ component/Hero.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className="relative flex items-center justify-center min-h-screen bg-[#000319]">
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+    <section className="relative flex items-center justify-center min-h-screen" initial="hidden" animate="visible">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute rotate-180 lg:top-[-360px] top-[-400px] left-0 z-[1]  lg:object-cover h-full w-full"
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video>
       <div className="relative z-10 p-8 text-center text-white">
         <h1 className="text-4xl sm:text-6xl font-bold">Hero Section</h1>
       </div>
