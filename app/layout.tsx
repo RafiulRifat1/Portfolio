506bc4790cf65dbb8ea3bba58d934ca4f3dcdefb
MD Rafiul Islam Rifat <rafiul2071@gmail.com>
2025-07-31 18:11:02 +0600

    hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -24,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
         {children}
       </body>
     </html>
--- component/Hero.tsx
+++ component/Hero.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import HeroContent from './content/HeroContent';
 
 const Hero = () => {
   return (
-    <section className="relative flex items-center justify-center min-h-screen" initial="hidden" animate="visible">
+    <section className="relative flex flex-col h-full w-full" id="about-me">
       <video
         autoPlay
         loop
         muted
-        className="absolute rotate-180 lg:top-[-360px] top-[-400px] left-0 z-[1]  lg:object-cover h-full w-full"
+        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
+      <HeroContent/>
       <div className="relative z-10 p-8 text-center text-white">
         <h1 className="text-4xl sm:text-6xl font-bold">Hero Section</h1>
       </div>
